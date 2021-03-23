36547a35c5580d9bc68686d2ca69a9fdbe729a69
iramos@digitalhouse.com <iramos@digitalhouse.com>
2021-03-23 00:53:56 +0000

    Mumuki Export on 2021-03-23 00:53:56 +0000

--- 00001_Lista de ímpares/test.py
+++ 00001_Lista de ímpares/test.py
@@ -3,3 +3,7 @@ class Test(unittest.TestCase):
   def test_deve_retornar_numeros_impares(self):
     input = [1,2,3,127,256,377]
     self.assertListEqual(impares(input), [1,3,127,377])
+    
+  def test_deve_retornar_vazio(self):
+    input = [2,256]
+    self.assertListEqual(impares(input), [])
